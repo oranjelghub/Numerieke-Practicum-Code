ed3c94e907946207513b2a606fa2bf4fb1db2180
Smogogole <goranhuygh@outlook.com>
2025-04-25 17:08:59 +0200

    Tweede code oef 3 af maar werkt niet

--- Test_zone.m
+++ Test_zone.m
@@ -1,3 +1,16 @@
 %Je kan hier variabele def en de functies uitproberen
 
-1+((10-1)/19).*(0:19)
+t = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+tbefore = [-2,-1,0];
+tafter = [15,16,17];
+
+f = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+[A,b] = stelselsplineint(t,f,tbefore,tafter);
+%disp(A)
+%disp(b)
+%disp(A\b)
+%length(A\b)
+
+
+evalsplineint(t,f,tbefore,tafter,20)
--- evalsplineint.m
+++ evalsplineint.m
@@ -3,25 +3,27 @@ function [y] = evalsplineint(t,f,tbefore,tafter,k)
 %Initiele defs
 L = length(t);
 y = 1:k;
+knp = [tbefore t tafter];
 
 %Stelsel opstellen en oplossen
 [A,b] = stelselsplineint(t,f,tbefore,tafter);
-coeff = A\b;
+coeff = A\b; %Bevat L+2 elementen
 
 %Lijst van equidistante punten
 evalpunt = t(1)+((t(L)-t(1))/(k-1)).*(0:k-1);
 
 %Interpolerende veelterm opstellen
-for i = 1:L
+for i = 1:length(evalpunt)
     sum = 0;
-    for j= 1:L %Moet andere indices zijn want niet de tijd gehad om te zien hoeveel coefficienten er waren
-        sum = sum + coeff(j);
+    for j= 1:length(coeff) 
+        sum = sum + coeff(j)*ppval(bspline([knp(j)
+                                            knp(j+1)
+                                            knp(j+2)
+                                            knp(j+3)
+                                            knp(j+4)]),evalpunt(i));
     end
     y(i) = sum;
 end
-
-
-
 end
 
 
