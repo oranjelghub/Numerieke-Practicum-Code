03cc60d88e1251e3e780474b916598bddf84c0c4
Smogogole <goranhuygh@outlook.com>
2025-04-30 22:20:57 +0200

    Kanker practicum heb net 2u gespendeerd op een functie fixen

--- Test_zone.m
+++ Test_zone.m
@@ -1,13 +1,11 @@
 %Knal hier code dat je wilt uittesten
 
+
 p = (1:20).^2;
 [A,b] = stelselsplineint(p,1:20,(-3:-1).^3,(21:23).^3);
-
 
 [L,U] = simple_lu(A);
 [l,u] = simple_lu_spline(A);
 
-
-disp(Backsub_U(U,Backsub_L(L,b))-(A\b)')
-
+disp(norm((A\b)' - Backsub_U(u,Backsub_L(l,b))))
 
--- simple_lu_spline.m
+++ simple_lu_spline.m
@@ -2,20 +2,19 @@ function [L,U] = simple_lu_spline(A)
 
 n = size(A,1);
 
-for k=1:n-1
+A(2,1) = A(2,1) / A(1,1) ;
+A(2,2) = A(2,2) - A(2,1) * A(1,2) ;
+A(2,3) = A(2,3) - A(2,1) * A(1,3) ;
 
+for k=2:n-3
     A(k+1,k) = A(k+1,k) / A(k,k) ;
-
-    if k<n-1
-
-        A(k+1:k+2,k+1:k+2) = A(k+1:k+2,k+1:k+2) - A(k+1:k+2,k) * A(k,k+1:k+2) ;
-        
-    else
-
-        A(k+1,k+1) = A(k+1,k+1) - A(k+1,k)*A(k+1,k);
-
-    end
+    A(k+1,k+1) = A(k+1,k+1) - A(k+1,k) * A(k,k+1) ;
 end
+
+A(n-1:n,n-2) = A(n-1:n,n-2)/A(n-2,n-2);
+A(n-1:n,n-1) = A(n-1:n,n-1) - A(n-1:n,n-2)*A(n-2,n-1);
+A(n,n-1) = A(n,n-1)/A(n-1,n-1);
+A(n,n) = A(n,n)-A(n,n-1)*A(n-1,n);
 
 L = tril(A,-1)+eye(size(A)) ;
 U = triu(A) ;
