21f913fed6ade7a1c916dfb1a255da6ed6d14e2f
Smogogole <goranhuygh@outlook.com>
2025-05-03 13:38:35 +0200

    Meer rond oef7

--- simple_lu_spline.m
+++ simple_lu_spline.m
@@ -1,20 +1,18 @@
 function [L,U] = simple_lu_spline(A)
 
-n = size(A,1);
-
 A(2,1) = A(2,1) / A(1,1) ;
 A(2,2) = A(2,2) - A(2,1) * A(1,2) ;
 A(2,3) = A(2,3) - A(2,1) * A(1,3) ;
 
-for k=2:n-3
+for k=2:size(A,1)-3
     A(k+1,k) = A(k+1,k) / A(k,k) ;
     A(k+1,k+1) = A(k+1,k+1) - A(k+1,k) * A(k,k+1) ;
 end
 
-A(n-1:n,n-2) = A(n-1:n,n-2)/A(n-2,n-2);
-A(n-1:n,n-1) = A(n-1:n,n-1) - A(n-1:n,n-2)*A(n-2,n-1);
-A(n,n-1) = A(n,n-1)/A(n-1,n-1);
-A(n,n) = A(n,n)-A(n,n-1)*A(n-1,n);
+A(end-1:end,end-2) = A(end-1:end,end-2)/A(end-2,end-2);
+A(end-1:end,end-1) = A(end-1:end,end-1) - A(end-1:end,end-2)*A(end-2,end-1);
+A(end,end-1) = A(end,end-1)/A(end-1,end-1);
+A(end,end) = A(end,end)-A(end,end-1)*A(end-1,end);
 
 L = tril(A,-1)+eye(size(A)) ;
 U = triu(A) ;
